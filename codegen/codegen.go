53a9eca623f1f6daebd9014aad10aefca05f70f7
Andrew Benton <andrew@sqlc.dev>
2023-11-02 11:35:37 -0700

    feat: Implement plugin RPC handler

--- codegen/codegen.go
+++ codegen/codegen.go
@@ -1,48 +1,14 @@
 package codegen
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"io"
-	"os"
 
 	pb "github.com/sqlc-dev/sqlc-go/plugin"
-	"google.golang.org/protobuf/proto"
+	"github.com/sqlc-dev/sqlc-go/rpc"
 )
 
 type Handler func(context.Context, *pb.GenerateRequest) (*pb.GenerateResponse, error)
 
 func Run(h Handler) {
-	if err := run(h); err != nil {
-		fmt.Fprintf(os.Stderr, "error generating output: %s", err)
-		os.Exit(2)
-	}
-}
-
-func run(h Handler) error {
-	var req pb.GenerateRequest
-	reqBlob, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return err
-	}
-	if err := proto.Unmarshal(reqBlob, &req); err != nil {
-		return err
-	}
-	resp, err := h(context.Background(), &req)
-	if err != nil {
-		return err
-	}
-	respBlob, err := proto.Marshal(resp)
-	if err != nil {
-		return err
-	}
-	w := bufio.NewWriter(os.Stdout)
-	if _, err := w.Write(respBlob); err != nil {
-		return err
-	}
-	if err := w.Flush(); err != nil {
-		return err
-	}
-	return nil
+	rpc.Handle(&server{handler: h})
 }
---
+++ codegen/server.go
@@ -0,0 +1,17 @@
+package codegen
+
+import (
+	"context"
+
+	pb "github.com/sqlc-dev/sqlc-go/plugin"
+)
+
+type server struct {
+	pb.UnimplementedCodegenServiceServer
+
+	handler Handler
+}
+
+func (s *server) Generate(ctx context.Context, req *pb.GenerateRequest) (*pb.GenerateResponse, error) {
+	return s.handler(ctx, req)
+}
